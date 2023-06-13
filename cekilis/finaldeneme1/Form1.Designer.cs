77d8efa4f9a31c09445d71a81a163d91e5a0ca53
enesdemir41 <49167863+enesdemir41@users.noreply.github.com>
2023-06-13 16:45:33 +0300

    Add files via upload

---
+++ cekilis/finaldeneme1/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿using System;
+
+namespace finaldeneme1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(116, 174);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "KATILIMCI EKLE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(28, 292);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(267, 212);
+            this.listBox1.TabIndex = 1;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(454, 67);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(199, 303);
+            this.listBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(124, 260);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "KATILIMCILAR";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(520, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "TALİHLİLER";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(92, 527);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 91);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "ÇEKİLİŞ YAP ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(142, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Kaç kişi Kazansın ?";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(157, 67);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 17);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Evet";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Yedek talihli olsun mu?";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(246, 67);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(49, 17);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Hayır";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged_1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 130);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "kaç talihli olsun ?";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(142, 130);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(904, 32);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "YEDEK TALİHLİLER";
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(860, 69);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(157, 134);
+            this.listBox3.TabIndex = 13;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(454, 537);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(199, 65);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Kazananları Göster";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1050, 649);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ cekilis/finaldeneme1/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Collections;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Reflection.Emit;
+
+namespace finaldeneme1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ArrayList katilimcilar = new ArrayList();                   // Katilimcilar isminde bir liste oluşturduk
+        string katilimciismi;                                       // ınputboxtan alınan kullanıcı isimleri kullaniciismi değişkeni olacak
+        int yedekno;                                                // yedek talihliler için rastegele sayıların tutulacağı değişkenimiz
+        int kazananno;                                              // asıl kazananlarımı için rastgele sayıların tutulacağı değişkenimiz
+        Random rastgelesayi = new Random();                         // rastegele sayı üretme 
+        int kackisi;                                                // kullanıcan alacagımız kac kazananın olacagının bilgisini tutan değişkenimiz
+        int talihli;                                                // kullanıcan alacagımız kac talihki olacagının bilgisini tutan değişkenimiz
+        ArrayList yedekKazananlar = new ArrayList();                // yedek kazananlarımız için isminde bir liste oluşturduk
+        string cikis;                                               // kullanıcıdan çıkış anahtırımızı oluşturmak için ve bu degeri tutmak için değişkenimiz
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            radioButton2.Checked = true;
+            button2.Visible = false;
+            listBox2.Visible = false;
+            label2.Visible = false;
+            label6.Visible = false;
+            listBox3.Visible = false;
+            label5.Visible = false;
+            textBox2.Visible = false;
+            button3.Visible = false;
+
+
+
+
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try {
+
+
+
+
+
+
+                cikis = Interaction.InputBox("Çeklişten çıkmak için bir anahtar girin", "Çekilişten Sonlandırma Anahtarı", "Buraya  giriniz", 200, 200).ToString(); // kullanıcı eklemeyi sonlardırmak için anahtarı ınputbox ile alacagız
+
+                for (; ; ) // sonuz döngümüzü oluşturduk çünkü kullanıcı istediği kadar kullanıcı ismi girebilsin diye
+                {
+
+
+                    katilimciismi = Interaction.InputBox("kullanıcı girişi", "Ekleme Ekranı", "Bu kısma isimleri giriniz", 200, 200);// ınputbox ile kullanıcıdan katılımcıları aldık
+                    if (katilimciismi == cikis || katilimciismi == cikis)// kullanıcın belirlediği bir anahtar ile donguden cık
+                    {
+                        button2.Visible = true; // başta gözükmeyen 
+                        break;
+
+                    }
+                    else
+                    {
+                        katilimcilar.Add(katilimciismi);// katılımcılar arrayine katılımcı isimlerini  ekle
+                        listBox1.Items.Add(katilimciismi);// lisboxta yazdır
+
+                    }
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Geçerli Formatta Sayı giriniz");
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("Girilen Tanımlı aralıkta değil");
+            }
+
+            catch (IndexOutOfRangeException)
+            {
+                MessageBox.Show("Belirtilenden fazla giriş yapıldı");
+            }
+
+
+
+        }
+
+
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            label5.Visible = true;
+            textBox2.Visible = true;
+        }
+
+        private void radioButton2_CheckedChanged_1(object sender, EventArgs e)
+        {
+            label5.Visible = false;
+            textBox2.Visible = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                if (radioButton1.Checked) // eğer kullanıcımız yedek kazananlar isterse assagıdakki kodlar calısacak
+                {
+
+                    kackisi = int.Parse(textBox1.Text); // kaç kişinin kazanacagını kullanıcan textbox1 ile aldık
+                    talihli = int.Parse(textBox2.Text); // kaç talihli olacagı bilgisini textboz2 ile kullanıcıdan aldık
+                    string[] yedekler = new string[talihli]; // yedeklar adında bir dizi oluştur yedeklerimiz bu dizi içinde olacak ve boyutu ise kullanıcın girdiği boyutta olacak
+                    string[] kazananlar = new string[kackisi];// kaç kişi kazancaksa o kadar yer açsın kazananlar adında bir dizi oluştur kazannanlarımız bu dizi içinde olacak ve boyutu ise kullanıcın girdiği boyutta olacak
+
+                    if (talihli > kackisi) // eğer yedek talihlilerimizin sayısı kazanacak kişi sayısından fazla olursa assada ki uyarı meydana gelecek
+                    {
+                        MessageBox.Show("Yedek Talihliler kazanacak sayısından fazla olamaz", "Uyarı", MessageBoxButtons.OK);
+                    }
+                    else   // eğer olmazsa assagıdaki kodlar calısacak
+                    {
+                        for (int i = 0; i <= kackisi - 1; i++) // döngümüzü kurduk kaç kişiye sayısı - 1  kadar çalışacak . -1 olmasının sebebi indekslerin 0 dan başlamasıdır 
+                        {
+
+                            kazananno = rastgelesayi.Next(0, katilimcilar.Count);// kazananlara rastgele numara verdik
+                            kazananlar[i] = katilimcilar[kazananno].ToString();// kazananlara  katilimcilar listesindeki kazananno adlıları ekle
+                            katilimcilar.RemoveAt(kazananno);// katilimcilar listesinden kaznannoları sil
+                            listBox2.Items.Add(kazananlar[i]); // kazananları lisbox2 de goster
+
+
+
+                        }
+
+
+
+
+
+
+                        for (int i = 0; i < talihli; i++)  // yukarıdaki işlemi birde yedek talihlilermiz için yaptık
+                        {
+
+                            //zaten katilimcilar adli listeden kaznanlarımız çıktığı güncellememize gerek yok 
+
+                            yedekno = rastgelesayi.Next(0, katilimcilar.Count);//yedeklere rastgele numara verdik
+                            yedekler[i] = katilimcilar[yedekno].ToString();    // yedeklere  katilimcilar listesindeki yedekno adlıları ekle
+                            katilimcilar.RemoveAt(yedekno);                    // katilimcilar listesinden yedeknoları sil
+                            listBox3.Items.Add(yedekler[i]);                   //// kazananları lisbox2 de goster
+
+                        }
+
+                        MessageBox.Show("Çekilişiniz Yapıldı");
+                        button3.Visible = true;
+                    }
+
+
+                }
+                else // kullanıcımız yedek kazananlar istemezse aşşagıdaki kodlarımız calısaacak
+                {
+
+                    kackisi = int.Parse(textBox1.Text);
+                    string[] kazananlar = new string[kackisi];// kaç kişi kazancaksa o kadar yer açsın
+                    for (int i = 0; i <= kackisi - 1; i++)
+                    {
+
+                        kazananno = rastgelesayi.Next(0, katilimcilar.Count);// kazananlara numara ver
+                        kazananlar[i] = katilimcilar[kazananno].ToString();// kazananların i. indeklilerini katilimcilar listesindeki kazananno adlıları ekle
+                        katilimcilar.RemoveAt(kazananno);// katilimcilar listesinden kaznannolaro sil
+                        listBox2.Items.Add(kazananlar[i]); // kazananları lisbox2 de goster
+
+
+                    }
+
+                    MessageBox.Show("Çekilişiniz Yapıldı");
+                    
+                    button3.Visible = true;
+
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Geçerli Formatta Sayı giriniz");
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("Girilen Tanımlı aralıkta değil");
+            }
+
+            catch (IndexOutOfRangeException)
+            {
+                MessageBox.Show("Belirtilenden fazla giriş yapıldı");
+            }
+
+
+
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            listBox2.Visible = true;
+            label2.Visible = true;
+            listBox3.Visible = true;
+            label6.Visible = true;
+        }
+    }
+
+
+  
+    
+}
+
